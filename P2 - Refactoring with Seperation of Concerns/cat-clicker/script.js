c92ac82435601c37e1a2311b572b91036cd1ce0c
nuhman <nuhman7@outlook.com>
2017-10-26 23:44:53 +0530

    change code style for js file

--- P2 - Refactoring with Seperation of Concerns/cat-clicker/script.js
+++ P2 - Refactoring with Seperation of Concerns/cat-clicker/script.js
@@ -5,94 +5,75 @@ $(function(){
   let model = {    
     // not concerned with where (which node) to display the data    
 
-    cats: [],   
+    currentCat: null,
 
-    imageUrls: [
-      'images/kitty.jpg',
-      'images/billy.jpg',
-      'images/alexis.jpg',
-      'images/roberts.jpg'      
-    ],
-
-    init(){
-       this.cats = [
-        { name: "KITTY", clickCount: 0, url: this.imageUrls[0] },
-        { name: "BILLY", clickCount: 0, url: this.imageUrls[1] },
-        { name: "ALEXIS", clickCount: 0, url: this.imageUrls[2] },
-        { name: "ROBERTS", clickCount: 0, url: this.imageUrls[3] }
-      ];
-    },
-
-    getAllCats(){
-      return this.cats;
-    }
+    cats: [
+      { name: "KITTY", clickCount: 0, url: 'images/kitty.jpg' },
+      { name: "BILLY", clickCount: 0, url: 'images/billy.jpg' },
+      { name: "ALEXIS", clickCount: 0, url: 'images/alexis.jpg' },
+      { name: "ROBERTS", clickCount: 0, url: 'images/roberts.jpg' }
+    ]
 
   };
 
   // Link Model and View Here
   let octopus = {
     
+    init(){
+      model.currentCat =  model.cats[0];
+      catListView.init();
+      catView.init();
+    },
+
+    getCurrentCat(){
+      return model.currentCat;
+    },
     getCats(){
-      return model.getAllCats();
+      return model.cats;
     },
-
-    // display the cat that was clicked
-    displayCat(self){      
-      $(".cat").hide();
-      this.getCats().forEach(function(cat, index){
-        if(self.textContent === cat.name){
-          $("#cat"+(index+1)).show();
-        }
-      });
+    setCurrentCat(cat){
+      model.currentCat = cat;
     },
-
-    // increment the clickCount of model, and update the view
-    countClicks(self){
-      let id = parseInt(self[0].id);     
-      $("#score"+(id+1)).text( ++(this.getCats()[id]).clickCount );      
+    incrementCounter(){
+      model.currentCat.clickCount++;
+      catView.render();
     }    
+  };
+// VIEWS
 
+  let catView = {
+    init(){  
+
+      $('.pic').click(function(){
+        octopus.incrementCounter();
+      });
+
+      this.render();
+    },
+    render(){
+      let currentCat = octopus.getCurrentCat();
+      $('.name').text(currentCat.name);
+      $('.pic').attr('src', currentCat.url);
+      $('.score').text(currentCat.clickCount);
+    }
+  };
+
+  let catListView = {
+    init(){       
+      this.render();
+    },
+    render(){
+      let cats = octopus.getCats();
+      cats.forEach(function(cat, index){
+        $('.list').append("<button class='btn' id='"+index+"' >"+cat.name+"</button>");
+        $('#'+index).click(function(){
+          octopus.setCurrentCat(cat);
+          catView.render();
+        })
+      });
+    }
   };
   
-  // View Starts
-  let view = {
-
-      init(){
-        this.catNode = $('.cats');
-        this.btnNode = $('.list');        
-
-        view.render();
-      },
-
-      render(){
-        let buttons = '';
-        let catDiv = '';
-        // render button
-        octopus.getCats().forEach(function(cat, index){
-            buttons += '<button class="btn" id="btn'+(index+1)+'" >'+
-                          cat.name +'</button>';
-            catDiv += '<div id="cat'+(index+1)+'" class="cat">'+
-                          '<p class="name">NAME: <span id="name'+(index+1)+'">'+cat.name+'</span></p>'+
-                          '<img src="'+cat.url+'" id="'+index+'" class="pic" />' +
-                          '<p class="score">CLICKS: <span id="score'+(index+1)+'">'+cat.clickCount+'</span></p>' +
-                      '</div>';
-        });
-
-        this.catNode.html(catDiv);
-        this.btnNode.html(buttons);
-
-        $('.btn').click(function(e){
-          octopus.displayCat(this);
-        });
-
-        $('.pic').click(function(e){
-          octopus.countClicks($(this));
-        });
-      }
-
-  };
-  
-  model.init(); // initialize data here!!
-  view.init(); // render view
+  octopus.init();
   
 }());
