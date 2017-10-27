ae8cbc34468cf51c714e716021de63220f794997
nuhman <nuhman7@outlook.com>
2017-10-27 07:50:03 +0530

    add admin options to page

--- P2 - Refactoring with Seperation of Concerns/cat-clicker/script.js
+++ P2 - Refactoring with Seperation of Concerns/cat-clicker/script.js
@@ -6,6 +6,7 @@ $(function(){
     // not concerned with where (which node) to display the data    
 
     currentCat: null,
+    showingAdmin: false,
 
     cats: [
       { name: "KITTY", clickCount: 0, url: 'images/kitty.jpg' },
@@ -22,7 +23,8 @@ $(function(){
     init(){
       model.currentCat =  model.cats[0];
       catListView.init();
-      catView.init();
+      catView.init();      
+      adminView.init();      
     },
 
     getCurrentCat(){
@@ -37,7 +39,36 @@ $(function(){
     incrementCounter(){
       model.currentCat.clickCount++;
       catView.render();
-    }    
+      adminView.render();
+    },
+
+    // ADMIN OPTIONS
+    toggleAdmin(){      
+      if(model.showingAdmin){
+        $(".admin-options").hide(1000);
+        model.showingAdmin = false;
+      } else {
+        $(".admin-options").show(1000);
+        model.showingAdmin = true;
+      }
+    },
+
+    updateDetails(name, url, clicks){
+        name = name.trim();
+        url = url.trim();
+        clicks = clicks.trim();        
+        if(name && url && clicks){
+          cat = model.currentCat;
+          cat.name = name;
+          cat.url = url;
+          cat.clickCount = clicks;
+          catListView.render();
+          catView.render();
+          adminView.render(); 
+        }
+    }
+    
+
   };
 // VIEWS
 
@@ -59,21 +90,50 @@ $(function(){
   };
 
   let catListView = {
-    init(){       
+    init(){             
       this.render();
     },
     render(){
+      $('.list').html('');
       let cats = octopus.getCats();
       cats.forEach(function(cat, index){
         $('.list').append("<button class='btn' id='"+index+"' >"+cat.name+"</button>");
         $('#'+index).click(function(){
           octopus.setCurrentCat(cat);
           catView.render();
+          adminView.render();
         })
       });
     }
   };
+
+  let adminView = {
+    init(){      
+      $(".admin-options").hide();
+
+      $("#admin").click(function(){
+        octopus.toggleAdmin();
+      });
+      $("#admin-cancel").click(function(){        
+        octopus.toggleAdmin();        
+      });
+      $("#admin-save").click(function(){                
+        let name = $("#edit-name-input").val();
+        let url = $("#edit-url-input").val();
+        let click = $("#edit-click-input").val();
+        octopus.updateDetails(name, url, click); 
+        octopus.toggleAdmin();               
+      });
+
+      this.render();
+    },
+    render(){
+      let cat = model.currentCat;
+      $("#edit-name-input").val(cat.name);
+      $("#edit-url-input").val(cat.url);
+      $("#edit-click-input").val(cat.clickCount);
+    }    
+  };
   
   octopus.init();
-  
 }());
