8e547f6cc144538d5f4ee0c63289cef268a913f5
nuhman <nuhman7@outlook.com>
2017-10-28 19:21:32 +0530

    seperate model and viewmodel

--- P2 - Refactoring with Seperation of Concerns/knockout-cat-clicker/js/app.js
+++ P2 - Refactoring with Seperation of Concerns/knockout-cat-clicker/js/app.js
@@ -1,19 +1,25 @@
-var viewModel = function(){
-    // model
+var Cat = function(){
     this.clickCount = ko.observable(0);
     this.name = ko.observable("Kitty");
     this.imgSrc = ko.observable("img/kitty.jpg");
     this.title = ko.observable("new born");
-
-    this.incrementCounter = function(){
-        this.clickCount( this.clickCount() + 1);        
-    };
-
+    this.nicknames = ko.observableArray([
+    "Tom","Cruise","Jeffy Kitty"
+    ]);
     this.updateTitle = ko.computed(function( ){
         if(this.clickCount() > 10 && this.clickCount() < 20) this.title("infant");
         else if(this.clickCount() >= 20) this.title("teenager");
     }, this);
-    
+}
+
+var viewModel = function(){    
+    let self = this;
+    self.currentCat = ko.observable(new Cat());
+    self.incrementCounter = function(){
+        self.currentCat().clickCount( self.currentCat().clickCount() + 1);        
+    };
+     
 };
 console.log("Running...");
+
 ko.applyBindings(new viewModel());
