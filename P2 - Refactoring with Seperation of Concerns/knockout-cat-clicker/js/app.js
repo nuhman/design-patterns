59aa5a58fe6c8f3a28160cb31482e808636cf4cb
nuhman <nuhman7@outlook.com>
2017-10-28 22:18:50 +0530

    change cat view on click

--- P2 - Refactoring with Seperation of Concerns/knockout-cat-clicker/js/app.js
+++ P2 - Refactoring with Seperation of Concerns/knockout-cat-clicker/js/app.js
@@ -1,25 +1,42 @@
-var Cat = function(){
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable("Kitty");
-    this.imgSrc = ko.observable("img/kitty.jpg");
-    this.title = ko.observable("new born");
-    this.nicknames = ko.observableArray([
-    "Tom","Cruise","Jeffy Kitty"
-    ]);
-    this.updateTitle = ko.computed(function( ){
-        if(this.clickCount() > 10 && this.clickCount() < 20) this.title("infant");
-        else if(this.clickCount() >= 20) this.title("teenager");
+var catsData = [
+    {name: "Billy", img: "billy.jpg", clicks: 0, nicknames: ["jeff", "tom"]},
+    {name: "Kitty", img: "kitty.jpg", clicks: 0, nicknames: ["Cutie", "Taco"]},
+    {name: "Katie", img: "katie.jpg", clicks: 0, nicknames: ["kat"]},
+    {name: "Roberts", img: "roberts.jpg", clicks: 0, nicknames: ["rob"]},    
+];
+
+var Cat = function(data){
+    this.clickCount = ko.observable(data.clicks);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable("img/"+data.img);    
+    this.nicknames = ko.observableArray(data.nicknames);
+    this.title = ko.computed(function(){
+        let title;
+        let clicks = this.clickCount();
+        if(clicks < 10) title = "New Born";
+        else if(clicks < 20) title = "Child";
+        else if(clicks < 30) title = "Teen";
+        else if(clicks < 40) title = "Voter";
+        else title = "Ninja";
+        return title;
     }, this);
-}
+};
+
+//TODO: show up list of cats in front page
 
 var viewModel = function(){    
-    let self = this;
-    self.currentCat = ko.observable(new Cat());
+    let self = this;   
+    self.catList = ko.observableArray([]);    
+    catsData.forEach(function(cat){
+        self.catList.push( new Cat(cat) );
+    });
+    self.currentCat = ko.observable( self.catList()[0] );
     self.incrementCounter = function(){
         self.currentCat().clickCount( self.currentCat().clickCount() + 1);        
     };
-     
+    self.changeCat = function(cat){        
+        self.currentCat(cat);
+    }        
 };
-console.log("Running...");
 
 ko.applyBindings(new viewModel());
